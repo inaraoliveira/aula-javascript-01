c364527c47d3bbdd8cbb4114896f887ce73926e8
Inara Oliveira <inaraolliveira23@gmail.com>
2021-09-17 14:48:35 -0300

    Atividades JS

---
+++ atividade1.js
@@ -0,0 +1,31 @@
+// Criar variáveis com tipos de dados: Number, String, Boolean, Array, Object, Undefined, Null.
+
+// Number
+var idade = 22;
+console.log(idade);
+
+// String
+var nome = "Inara";
+console.log(nome);
+
+// Boolean
+var tenhoDinheiro = false;
+console.log(tenhoDinheiro);
+
+// Array
+var frutas = ["Morango", "Kiwi", "Amora"];
+console.log(frutas);
+
+// Object
+var inara = {idade: 21, altura: 1.67};
+console.log(inara);
+console.log(inara.idade);
+console.log(inara.altura);
+
+// Undefined
+var indefinido;
+console.log(indefinido);
+
+// Null
+var vazio = null;
+console.log(vazio);
---
+++ atividade2.js
@@ -0,0 +1,23 @@
+// Variável var
+
+var nome = "Inara";
+console.log(nome);
+
+var idade = 21;
+console.log(idade);
+
+// Variável let
+
+let nome = "Eduardo";
+console.log(nome);
+
+let idade = 22;
+console.log(idade);
+
+// Variável const
+
+const CPF = 12345678900;
+console.log(CPF)
+
+const RG = 12345678;
+console.log(RG);
